8ff3f6a5b307c1a4a293f28b14b6d08240c4f75d
Matthew McAlarney <67719286+MacCode7110@users.noreply.github.com>
2023-10-26 00:00:53 -0400

    Started Exercise 2.9

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 #CS 4341 Assignment A1
 #Matthew McAlarney
+
+#Important Notes:
+    #main.py contains Exercise 2.9
---
+++ main.py
@@ -0,0 +1,10 @@
+# Exercise 2.9
+
+# Implement a simple reflex agent for the vacuum environment in Exercise 2.8.
+# Run the environment with this agent for all possible initial dirt configurations and agent locations.
+# Record the performance score for each configuration and the overall average score.
+
+# 1X2 vacuum cleaner space according to Figure 2.2
+# Initial configurations and locations are (A, Clean), (A, Dirty), (B, Clean), (B, Dirty)
+
+
